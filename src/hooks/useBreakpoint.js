56eeaea5e4da28c905769287570b2231bc92a887
Maxim290 <maxim.mironovdev@gmail.com>
2025-04-09 16:24:26 +0200

    fix useBreakpoint hook

--- src/hooks/useBreakpoint.js
+++ src/hooks/useBreakpoint.js
@@ -4,28 +4,28 @@ export function useBreakpoint() {
     const [breakpoint, setBreakpoint] = useState("xl");
 
     const handleResize = () => {
-        const width = window.innerWidth;
-
-        if (width <= 530) {
-            setBreakpoint("xs");
-        } else if (width <= 770) {
-            setBreakpoint("sm");
-        } else if (width <= 1200) {
-            setBreakpoint("md");
-        } else if (width <= 1440) {
-            setBreakpoint("lg");
-        } else if (width <= 1920) {
-            setBreakpoint("xl");
-        } else {
+        if (window.matchMedia("(min-width: 1921px)").matches) {
             setBreakpoint("xxl");
+        } else if (window.matchMedia("(min-width: 1441px)").matches) {
+            setBreakpoint("xl");
+        } else if (window.matchMedia("(min-width: 1201px)").matches) {
+            setBreakpoint("lg");
+        } else if (window.matchMedia("(min-width: 771px)").matches) {
+            setBreakpoint("md");
+        } else if (window.matchMedia("(min-width: 531px)").matches) {
+            setBreakpoint("sm");
+        } else {
+            setBreakpoint("xs");
         }
     };
 
     useEffect(() => {
-        handleResize();
+        const update = () => handleResize();
 
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
+        update();
+
+        window.addEventListener("resize", update);
+        return () => window.removeEventListener("resize", update);
     }, []);
 
     return breakpoint;
